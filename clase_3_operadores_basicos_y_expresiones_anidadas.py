a2d4f2bfaa01769e3998beced0c7b4963b9ff3de
Germán Martinez <germand_m@hotmail.com>
2022-08-18 20:03:34 -0300

    ejercicios de clase 3

---
+++ clase_3_operadores_basicos_y_expresiones_anidadas.py
@@ -0,0 +1,68 @@
+#Expresiones Aritmeticas
+
+2 + 3
+
+#Expresiones Algebraicas
+
+a=1
+a + 2
+
+#Boolean/Boleano/Lógico/Binary/Binario
+True
+False
+
+#Negacion
+not True  # False
+not False # True
+
+# Comparadores Números
+1 + 1 == 2 # True
+1 + 1 == 3 # False
+
+1 != 2 # True
+1 != 2 # False
+
+1 < 3  # True
+3 <= 3 # True
+4 < 1  # False
+4 <= 1 # False
+
+2 > 3  # True
+3 >= 3 #True
+5 > 8  # False
+5 >= 8 #False
+
+#Operadores String
+"hola" == "hola" # True
+"hola" == "hoLa" # False
+
+# And
+True and False # False
+a = 1
+0 == (a * 0) and a == (33/33) and (5 == 1 + a ) # True
+
+
+# Or
+False or False == False
+b = 1
+b == 1 or b == 2 # True
+b == 5 or b == 2 # False
+
+#Not
+
+not (True and False) # True
+not (False or False) # True
+
+not (True and False) == (not True or not False) # True
+
+not(False or False) == (not False and not False) # True
+
+# Operador de Asignación
+c = 10
+c += 1
+c -= 1
+c /= 2
+c %= 2
+c *= 2
+c **= 2
+
