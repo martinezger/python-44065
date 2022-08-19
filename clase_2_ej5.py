14fa7a8d0d69a5455bae7a3a6b9a28de1e8dd626
Germán Martinez <germand_m@hotmail.com>
2022-08-18 23:35:22 -0300

    ejercicio entregable

---
+++ clase_2_ej5.py
@@ -0,0 +1,14 @@
+## Ej 5 Desafio Clase 2
+
+a = [1, 5, 1]
+b = [2, 1, 2]
+c = [3, 0, 1]
+d = [1, 4, 4]
+
+matriz = [a,b,c,d]
+
+matriz_resultado = [a + [sum(a)],
+                    b + [sum(b)],
+                    c + [sum(c)],
+                    d + [sum(d)]]
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
