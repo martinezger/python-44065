5342c3a263233c362d3813088707b8511282d2d1
Germán Martinez <germand_m@hotmail.com>
2022-08-23 21:21:50 -0300

    arreglo

--- clase_4_controladores_de_flujo.py
+++ clase_4_controladores_de_flujo.py
@@ -4,17 +4,16 @@ if edad >= 18:
     print(f"{edad}, Es mayor de edad")
 
 
-#Checkear si Es mayor de 18 y es Extranjero (No es Argentino)
+#Checkear si Es mayor de 18 y es Argentino
 edad = 18
 pais = "Arg"
 
 if edad >= 18:
     print(f"{edad}, Es mayor de edad")
 
-if not (pais == "Arg"):
+if pais == "Arg":
     print("Es extrajera")
-else:
-    print("Es Argentino")
+
     
 
 #Checkear si es Argetino y si puede comprar Alcohol
@@ -25,13 +24,18 @@ if edad >= 18:
   print("Es Mayor a 18.")
   if pais == "Arg":
     print("Puede comprar alcohol.")
-  else:
-    print("No es Argentino, no puede comprar alcohol")
-else:
-    print("No es mayot de 18")
 
 
 #Checkar si Puede votar en Argentina
+
+edad =  int(input("Ingrese edad:"))
+pais  =  input("Ingrese país:") 
+if pais == "Arg":
+    if edad >= 16:
+        print("Puede votar")
+
+# se puede resumir en
+
 edad = 16
 pais = "Arg"
 
@@ -40,34 +44,28 @@ if edad >= 16 and pais == "Arg":
 
 
 #Chekear si es alto
-altura = 1.9
-if altura > 1.9: 
-  print("Es Alto") ## True
+pais = "Arg"
+if altura > "Arg": 
+  print("Es Arg") ## True
 else:  
-  print("No es Alto") ## False
+  print("No es Arg") ## False
 
 
 #Validar nota
-nota = 9
-if nota == 10:
-    print("Excelente")
-    
+
 if nota >= 9 :
-  print("Sobresaliente")
-
-if nota >= 7 :
-  print("Bueno")
-else:
-  print("Insuficiente")
-
-
-
-# Validar Nota
-if nota == 10:
-    print("Excelente")
-elif nota >= 9 :
   print("Sobresaliente")
 elif nota >= 7 :
   print("BUeno")
+else:
+  print("Insuficiente")
+
+#
+
+nota = 9    
+if nota >= 9 :
+  print("Sobresaliente")
+if nota >= 7 :
+  print("Bueno")
 else:
   print("Insuficiente")
