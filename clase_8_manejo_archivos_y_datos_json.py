a27b3dd928fa3b8aeff120673f03b9292fc2794e
Germán Martinez <german.martinez.c@getflex.com>
2022-09-06 11:54:20 -0300

    archivos y datos

--- clase_8_manejo_archivos_y_datos_json.py
+++ clase_8_manejo_archivos_y_datos_json.py
@@ -3,11 +3,15 @@ import json
 
 #Rick & Morty Api
 
-
+# Obtengo un mensaje en formato json desde un servidor
 print("====Buscar por Id de personaje=====")
 respuesta = urllib.request.urlopen("https://rickandmortyapi.com/api/character/1")
 body = json.loads(respuesta.read())
 print(json.dumps(body, indent=2))
+
+#Guardo el mensaje en formato texto como sample.json
+with open("sample.json", "w") as archivo:
+    archivo.writelines(json.dumps(body, indent=2))
 
 
 print("====Buscar por Nombre de personaje=====")
