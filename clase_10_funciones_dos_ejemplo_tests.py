49f973127da98dd9a77e19097156cf3874314bc9
Germán Martinez <45072884+martinezger@users.noreply.github.com>
2022-09-14 22:45:53 -0300

    Update clase_10_funciones_dos_ejemplo_tests.py

--- clase_10_funciones_dos_ejemplo_tests.py
+++ clase_10_funciones_dos_ejemplo_tests.py
@@ -8,7 +8,7 @@ assert recortar(num=7, inferior=5, superior=8) == 7
 def separar(numeros_enteros):
     pass
 
-assert separar([1,4,2,5]) == ([4,2], [1,5])
+assert separar([1,4,2,5]) == ([2,4], [1,5])
 assert separar([1,1,1,1]) == ([], [1,1,1,1])
 assert separar([2,2,2,2]) == ([2,2,2,2], [])
 assert separar([]) == ([],[])
